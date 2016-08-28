9735348b3864197e8c9e0db0e4816d80a6e768c6
An Le <thienan289@gmail.com>
2016-08-29 06:55:14 +0700

    revert api configuration

--- app/js/config.js
+++ app/js/config.js
@@ -5,14 +5,7 @@ angular.module('config', [])
     debug: false,
     apiHost: (
         function () {
-
-            //return 'http://iquantifi.azurewebsites.net/api';
-
-            if (window.location.hostname.toUpperCase() == 'localhost'.toUpperCase())
-                return 'http://localhost:53205/api';
-            else
-                return window.location.protocol + '//' + window.location.hostname + '/api';
-
+            return 'http://iquantifi.azurewebsites.net/api';
         })(),
     fbAppId: '648677845173289'
 })
