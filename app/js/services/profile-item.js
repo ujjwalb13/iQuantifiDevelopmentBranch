521efaa5650ecf39e093879d460d5280a76a9f42
vu.vo <vu.vo@codebox.vn>
2016-08-29 23:38:18 +0700

    complete my profile - show incomplete number on dashboard

--- app/js/services/profile-item.js
+++ app/js/services/profile-item.js
@@ -2,14 +2,17 @@
 angular.module('agera').factory('ProfileItem', ['Resource', '$http', 'ENV',
   function(resource, http, ENV) {
     return resource(
-      ENV.apiHost + "/ProfileItems/:itemId", {
+      ENV.apiHost + "/ProfileItems/:itemId",
+      {
         itemId: '@itemId'
       }, {
         query: {
           isArray: false
+        },
+        count: {
+          url: ENV.apiHost + '/ProfileItems/Count',
+          isArray: false
         }
-      }
-
-    );
+      });
   }]
 );
