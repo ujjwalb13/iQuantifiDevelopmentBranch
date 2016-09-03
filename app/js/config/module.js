4422db19c1bbd635e740ba6c66d6d38fb0f18faa
vu.vo <vu.vo@codebox.vn>
2016-09-03 12:17:28 +0700

    remove unused vars

--- app/js/config/module.js
+++ app/js/config/module.js
@@ -1,5 +1,4 @@
 'use strict';
-var app, dependencies;
 
 angular.module('actions', []);
 
@@ -19,11 +18,11 @@ angular.module('progress', []);
 
 angular.module('myMoney', []);
 
-dependencies = ['ngAnimate', 'ngMessages', 'ngRoute', 'ngResource', 'ngSanitize', 'ngTouch',
+var dependencies = ['ngAnimate', 'ngMessages', 'ngRoute', 'ngResource', 'ngSanitize', 'ngTouch',
   'ngMaterial', 'ui.bootstrap', 'ui.utils', 'ui.slider', 'Devise', 'angulartics.google.tagmanager',
   'emguo.poller', 'slick', 'matchmedia-ng', 'newrelic-timing', 'actions', 'admin', 'aggregation',
   'goals', 'onboard', 'summaries', 'timeline', 'config', 'onboard-nav', 'experience-picker',
   'profile-sidebar', 'money-sidebar', 'feature-sidebar', 'mgo-angular-wizard', 'progress',
   'myMoney', 'toaster', 'progress-sidebar'];
 
-app = angular.module('agera', dependencies);
+angular.module('agera', dependencies);
