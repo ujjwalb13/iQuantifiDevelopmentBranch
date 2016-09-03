720cfdd36fe056ef99376a1965c1f70ff5cad2a4
An Le <thienan289@gmail.com>
2016-09-03 16:09:17 +0700

    fetch goals

--- app/js/controllers/progress/my-achievements.js
+++ app/js/controllers/progress/my-achievements.js
@@ -1,7 +1,21 @@
 (function() {
   'use strict';
-  angular.module('progress').controller('MyAchievementsCtrl', function($scope, $location, $routeParams) {
-    console.log("11111");
+  angular.module('progress').controller('MyAchievementsCtrl', function($scope, $location, $routeParams, MyAchievements) {
+    MyAchievements.get().$promise.then(function(data) {
+      var sections = data.Sections;
+      var goalsSection = _.find(sections, function(item) {
+        return item.SectionType.toLowerCase() === "goal";
+      });
+
+      if (_.isObject(goalsSection)) {
+        $scope.goal = {
+          sectionName: goalsSection.SectionName,
+          items:  _.map(goalsSection.Goals, function(item) {
+            return { name: item.name, amount: item.amount, date: item.completed_date, type: item.type };
+          })
+        };
+      }
+    });
   });
 }).call(this);
 
