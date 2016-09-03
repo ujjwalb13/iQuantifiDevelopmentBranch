d68edb15d6ff7baf2ade6a58df364ce908367a82
vu.vo <vu.vo@codebox.vn>
2016-09-03 13:17:35 +0700

    watch file changes

--- docker/compass/compile-css.sh
+++ docker/compass/compile-css.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-compass compile -s compressed --sass-dir scss/ --force
+echo "start watching: $(pwd)"
+inotifywait -rm -e modify ./scss | while read; do
+  compass compile -s compressed --sass-dir scss/ --force
+done
