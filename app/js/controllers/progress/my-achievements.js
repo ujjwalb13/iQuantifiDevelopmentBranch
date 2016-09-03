b2bd4ce7adfbedc8ebd28e01f2c2ea480ed915ee
An Le <thienan289@gmail.com>
2016-09-03 16:45:17 +0700

    add protection

--- app/js/controllers/progress/my-achievements.js
+++ app/js/controllers/progress/my-achievements.js
@@ -1,6 +1,15 @@
 (function() {
   'use strict';
   angular.module('progress').controller('MyAchievementsCtrl', function($scope, $location, $routeParams, MyAchievements) {
+
+    var protectionType = function(protection){
+      var type = protection.type.toLowerCase();
+      if (type === "reserve") {
+        return protection.kind.toLowerCase();
+      }
+      return type;
+    };
+
     MyAchievements.get().$promise.then(function(data) {
       var sections = data.Sections;
       var goalsSection = _.find(sections, function(item) {
@@ -15,6 +24,20 @@
           })
         };
       }
+
+      var protectionsSection = _.find(sections, function(item) {
+        return item.SectionType.toLowerCase() === "debt";
+      });
+
+      if (_.isObject(protectionsSection)) {
+        $scope.protectionsSection = {
+          name: protectionsSection.SectionName,
+          items:  _.map(protectionsSection.Goals, function(item) {
+            return { name: item.name, amount: item.amount, date: item.completed_date, type: protectionType(item) };
+          })
+        };
+      }
+
     });
   });
 }).call(this);
