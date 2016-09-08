e02a44f8b947a5386493bbc3363c26842b8b10e5
vu.vo <vu.vo@codebox.vn>
2016-09-08 22:32:47 +0700

    show find cash

--- app/js/controllers/my-features/MyFeaturesCashFinderCtrl.js
+++ app/js/controllers/my-features/MyFeaturesCashFinderCtrl.js
@@ -3,122 +3,41 @@
   angular.module('agera').controller('MyFeaturesCashFinderCtrl', function($scope, $routeParams, $rootScope, $http, $location, $modal, Expense, Scenario, CashfinderService, ENV) {
     var TaxModalInstanceCtrl;
     $rootScope.$broadcast('clearAlerts');
-    if ($rootScope.expenses == null) {
+
+    CashfinderService.fetchData().then(function(){
+      if ($rootScope.shortage > 0) {
+        $rootScope.showExpenses = true
+      } else {
+        $rootScope.showExpenses = false
+      }
+    });
+
+    $scope.expense = {}
+
+    $scope.findCash = function(isValid) {
+      $scope.submitted = true
+      if (!isValid)
+        return false
+      $scope.processing = true;
+
       CashfinderService.fetchData().then(function(){
-        $scope.selectedExpense = $rootScope.expenses[0];
-      });
+        $rootScope.expenses = _.filter($rootScope.expenses, function(expense){
+          return expense.amount <= $scope.expense.amount;
+        })
+        if ($rootScope.expenses.length > 0) {
+          $rootScope.showExpenses = true;
+        } else {
+          $rootScope.showExpenses = false;
+        }
+        $scope.processing = false;
+        $scope.submitted = false;
+      })
     }
 
     $scope.selectExpense = function(expense) {
       $scope.selectedExpense = expense
     }
 
-    $scope.openNeedTaxModal = function() {
-      var modalInstance;
-      return modalInstance = $modal.open({
-        templateUrl: 'needTaxModal',
-        controller: TaxModalInstanceCtrl,
-        windowClass: 'new-iq-modal yellow'
-      });
-    };
-    $scope.openHaveTaxModal = function() {
-      var modalInstance;
-      return modalInstance = $modal.open({
-        templateUrl: 'haveTaxModal',
-        controller: TaxModalInstanceCtrl,
-        windowClass: 'new-iq-modal yellow'
-      });
-    };
-    TaxModalInstanceCtrl = [
-      '$scope', '$modalInstance', '$http', function($scope, $modalInstance, $http) {
-        $scope.taxRefundAmount = 0;
-        $scope.close = function() {
-          return $modalInstance.dismiss('cancel');
-        };
-        $scope.submit = function(amt) {
-          $scope.processing = true;
-          return $http.post(ENV.apiHost + "/tax_refunds", {
-            year: moment().subtract(1, 'years').year(),
-            amount: amt
-          }).then(function() {
-            return Scenario.shortage().$promise.then(function(data) {
-              var newShortage;
-              newShortage = data.shortage;
-              if (newShortage > 0) {
-                return $scope.openNotEnoughModal();
-              } else {
-                return $scope.openEnoughModal();
-              }
-            });
-          })["finally"](function() {
-            var processing;
-            return processing = false;
-          });
-        };
-        $scope.openTaxRefundModal = function() {
-          var modalInstance;
-          $modalInstance.close();
-          return modalInstance = $modal.open({
-            templateUrl: 'taxRefundModal',
-            controller: TaxModalInstanceCtrl,
-            windowClass: 'new-iq-modal yellow'
-          });
-        };
-        $scope.openEnoughModal = function() {
-          var modalInstance;
-          $modalInstance.close();
-          return modalInstance = $modal.open({
-            templateUrl: 'enoughModal',
-            controller: TaxModalInstanceCtrl,
-            windowClass: 'new-iq-modal yellow'
-          });
-        };
-        $scope.openNotEnoughModal = function() {
-          var modalInstance;
-          $modalInstance.close();
-          return modalInstance = $modal.open({
-            templateUrl: 'notEnoughModal',
-            controller: TaxModalInstanceCtrl,
-            windowClass: 'new-iq-modal yellow'
-          });
-        };
-        $scope.openNoRefundModal = function() {
-          var modalInstance;
-          $http.post(ENV.apiHost + "/tax_refunds", {
-            year: moment().subtract(1, 'years').year(),
-            amount: 0
-          });
-          $modalInstance.close();
-          return modalInstance = $modal.open({
-            templateUrl: 'noRefundModal',
-            controller: TaxModalInstanceCtrl,
-            windowClass: 'new-iq-modal yellow'
-          });
-        };
-        $scope.goToDashboard = function() {
-          $modalInstance.close();
-          return $location.path('/');
-        };
-        return $scope.goToWhatIf = function() {
-          $modalInstance.close('close');
-          return $location.path('/what-if');
-        };
-      }
-    ];
-    $scope.taxRefundAmount = 0;
-    $http.get(ENV.apiHost + "/tax_refunds/current").then(function(data) {
-      $scope.taxRefundAmount = data.data;
-      if (CashfinderService.showModal) {
-        if (!$scope.taxRefundAmount) {
-          return $scope.openNeedTaxModal();
-        } else {
-          return $scope.openHaveTaxModal();
-        }
-      }
-    });
-    $scope.goToEdit = function(id) {
-      return $location.path("/cashfinder/" + id);
-    };
     $scope.reset = function() {
       $rootScope.overrides = {};
       $rootScope.showAllExpenses = false;
@@ -161,12 +80,10 @@
     var fetchSubcategories;
     fetchSubcategories = function(name) {
       return $http.get(ENV.apiHost + "/finance/expenses/subcategories/" + name).then(function(response) {
-        console.log('sub', response)
         return $scope.subcategories = response.data;
       });
     };
     $scope.$watch('selectedExpense', function(newValue, oldValue){
-      console.log(newValue, oldValue);
       if (newValue != null && newValue != oldValue) {
         $scope.expense = newValue;
         fetchSubcategories($scope.expense.kind);
--- app/js/services/cashfinder.js
+++ app/js/services/cashfinder.js
@@ -8,38 +8,20 @@
         return $http.post(ENV.apiHost + "/advice/cashfinder", {
           overrides: $rootScope.overrides
         }).then(function(data) {
-          var expense, i, j, k, len, len1, len2, ref, ref1, ref2, results;
-          $rootScope.expenses = data.data.expenses;
+          $rootScope.expenses = _.filter(data.data.expenses, function(expense){
+            return expense.amount });
+
           $rootScope.shortage = data.data.shortage;
-          if ($rootScope.overrides == null) {
-            $rootScope.overrides = {};
-          }
-          ref = $rootScope.expenses;
-          for (i = 0, len = ref.length; i < len; i++) {
-            expense = ref[i];
-            expense.new_amount = expense.amount;
-          }
-          ref1 = $rootScope.expenses;
-          for (j = 0, len1 = ref1.length; j < len1; j++) {
-            expense = ref1[j];
-            if (expense.recommended_amount && ($rootScope.overrides[expense.kind] == null)) {
-              expense.new_amount = expense.recommended_amount;
-            }
-          }
-          ref2 = $rootScope.expenses;
-          results = [];
-          for (k = 0, len2 = ref2.length; k < len2; k++) {
-            expense = ref2[k];
-            if ($rootScope.overrides[expense.kind] != null) {
-              results.push(expense.new_amount = $rootScope.overrides[expense.kind]);
-            }
-          }
-          return results;
+
+          $rootScope.overrides = $rootScope.overrides || {};
+
+          _.each($rootScope.expenses, function(expense){
+            expense.new_amount = expense.recommended_amount || expense.amount;
+          });
         });
       }
     };
   });
-
 }).call(this);
 
 //# sourceMappingURL=cashfinder.js.map
