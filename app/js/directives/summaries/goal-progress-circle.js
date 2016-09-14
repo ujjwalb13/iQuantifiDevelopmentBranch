2775337d96ad796265d7599e09f54de1e00cc1de
An Le <thienan289@gmail.com>
2016-09-14 07:16:41 +0700

    correct donut

--- app/js/directives/summaries/goal-progress-circle.js
+++ app/js/directives/summaries/goal-progress-circle.js
@@ -13,9 +13,6 @@
       return d.endAngle + 0;
     });
 
-    console.log("4444");
-
-
     return {
       restrict: 'E',
       replace: true,
@@ -28,33 +25,33 @@
       },
       link: function(scope, element, attrs) {
         var svg = d3.select(element[0]).append('svg').attr('width', width).attr('height', height).append('g').attr('transform', "translate(" + (width / 2) + ", " + (height / 2) + ")");
+        svg.append('path').datum({
+            startAngle: 0,
+            endAngle: 2 * Math.PI
+          }).style('fill', '#e5e0c7')
+            .attr('d', arc);
+
         scope.behindStatusClass = function() {
-          if (scope.status === 'behindTwoMonth') return "behind-two-month";
-          if (scope.status === 'behindOneMonth') return "behind-one-month";
+          if (scope.status === 'danger') return "behind-two-month";
+          if (scope.status === 'warning') return "behind-one-month";
           return ""
         }
 
         return scope.$watch('complete', function(newValue, oldValue) {
-          var arcTween, background, complete, completeEndAngle, incomplete, incompleteColor;
+          var arcTween, complete, completeEndAngle, incomplete, incompleteColor;
           if ((newValue == null) || (typeof isNaN === "function" ? isNaN(newValue) : void 0)) {
             return;
           }
           if (scope.incomplete == null) {
             scope.incomplete = 0;
           }
-          if (scope.status === 'behindTwoMonth') {
-            incompleteColor = '#e16250';
-          } else {
+          if (scope.status === 'warning') {
             incompleteColor = '#ffc744';
+          } else {
+            incompleteColor = '#e16250';
           }
 
-
           completeEndAngle = (scope.complete / 100) * 2 * Math.PI;
-          background = svg.append('path').datum({
-            startAngle: 0,
-            endAngle: 2 * Math.PI
-          }).style('fill', '#e5e0c7')
-            .attr('d', arc);
           complete = svg.append('path').datum({
             startAngle: 0,
             endAngle: 0
