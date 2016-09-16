e2cdb364489450a38ad60085ad609ad9b1177d92
An Le <thienan289@gmail.com>
2016-09-16 21:53:05 +0700

    add delete modal

--- app/js/directives/goals/delete-modal.js
+++ app/js/directives/goals/delete-modal.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  angular.module('agera').directive('goalsDeleteModal', function() {
+  angular.module('agera').directive('goalsDeleteModal', function($modal) {
     return {
       restrict: 'EA',
       templateUrl: '/views/directives/goals/delete-modal.tpl.html',
@@ -8,7 +8,46 @@
         goal: '='
       },
       link: function(scope) {
-        scope.showModal = function () {
+        var ModalInstanceCtrl = [
+          '$location', '$scope', '$modalInstance', 'goalService', 'goal', function($location, $scope, $modalInstance, goalService, goal) {
+            $scope.goal = goal;
+
+            $scope.close = function() {
+              return $modalInstance.dismiss('cancel');
+            };
+
+            $scope.delete = function() {
+              $scope.pending = true;
+              return goalService["delete"]($scope.goal).then(function(response) {
+                $modalInstance.close("okay");
+                $rootScope.$broadcast('alert', {
+                  type: 'success',
+                  msg: 'The goal was deleted successfully.'
+                });
+              }, function(response) {
+                $modalInstance.dismiss('cancel');
+                return $rootScope.$broadcast('alert', {
+                  type: 'danger',
+                  msg: 'There was an error deleting the goal.'
+                });
+              })["finally"](function() {
+                return $scope.pending = false;
+              });
+            };
+          }
+        ];
+
+        scope.showModal = function (goal) {
+          var modalInstance;
+          return modalInstance = $modal.open({
+            templateUrl: 'deleteGoalModal',
+            controller: ModalInstanceCtrl,
+            resolve: {
+              goal: function() {
+                return goal;
+              }
+            }
+          });
         }
       }
     };
